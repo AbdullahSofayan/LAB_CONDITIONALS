20e724aa3b136ff0bf42ffd4ba1c267fe963f2cd
Abdullah Alsofayan <alsofayan10@gmail.com>
2025-06-18 12:17:59 +0300

    solve Bouns1

---
+++ Bouns1.py
@@ -0,0 +1,11 @@
+
+
+Wieght = float (input("Enter Your Weight: "))
+Height = float (input("Enter Your Height (in meters): "))
+BMI = Wieght / (Height ** 2)
+print("Your BMI:", round(BMI,2))
+
+
+
+
+
