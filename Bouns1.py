353368cc0bca6855a3ea96aa637845082012f167
Abdullah Alsofayan <alsofayan10@gmail.com>
2025-06-18 12:22:54 +0300

    add conditionals

--- Bouns1.py
+++ Bouns1.py
@@ -5,6 +5,12 @@ Height = float (input("Enter Your Height (in meters): "))
 BMI = Wieght / (Height ** 2)
 print("Your BMI:", round(BMI,2))
 
+if BMI > 25:
+    print("You are overweight. You need to work out more and watch your diet.")
+elif 18.5 <= BMI <= 25:
+    print("You are fit & healthy.")
+else:
+    print("You are underweight. Watch your health.")
 
 
 
